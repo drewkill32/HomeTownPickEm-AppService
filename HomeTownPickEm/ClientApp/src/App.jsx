bcd8eccc6c335a300319a1ee03abfed893dff0fc
Drew Killion <adk@killion.me>
2021-08-13 19:59:29 -0400

    adding components to functions

---
+++ HomeTownPickEm/ClientApp/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route } from "react-router";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import { Counter } from "./components/Counter";
+import AuthorizeRoute from "./components/api-authorization/AuthorizeRoute";
+import ApiAuthorizationRoutes from "./components/api-authorization/ApiAuthorizationRoutes";
+import { ApplicationPaths } from "./components/api-authorization/ApiAuthorizationConstants";
+
+import "./custom.css";
+
+const App = () => {
+  return (
+    <Layout>
+      <Route exact path="/" component={Home} />
+      <Route path="/counter" component={Counter} />
+      <AuthorizeRoute path="/fetch-data" component={FetchData} />
+      <Route
+        path={ApplicationPaths.ApiAuthorizationPrefix}
+        component={ApiAuthorizationRoutes}
+      />
+    </Layout>
+  );
+};
+
+export default App;
---
+++ HomeTownPickEm/ClientApp/src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "reactstrap";
+import NavMenu from "./NavMenu";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <NavMenu />
+      <Container>{children}</Container>
+    </div>
+  );
+};
+
+export default Layout;
