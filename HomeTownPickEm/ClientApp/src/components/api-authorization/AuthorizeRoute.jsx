030941bf129da0de73ef43deced749764b0ff21c
Drew Killion <adk@killion.me>
2021-08-14 10:32:43 -0400

    making a function

--- HomeTownPickEm/ClientApp/src/components/api-authorization/AuthorizeRoute.jsx
+++ HomeTownPickEm/ClientApp/src/components/api-authorization/AuthorizeRoute.jsx
@@ -1,56 +1,54 @@
-import React from 'react'
-import { Component } from 'react'
-import { Route, Redirect } from 'react-router-dom'
-import { ApplicationPaths, QueryParameterNames } from './ApiAuthorizationConstants'
-import authService from './AuthorizeService'
+import React, { useState, useEffect } from "react";
+import { Route, Redirect } from "react-router-dom";
+import {
+  ApplicationPaths,
+  QueryParameterNames,
+} from "./ApiAuthorizationConstants";
+import authService from "./AuthorizeService";
 
-export default class AuthorizeRoute extends Component {
-    constructor(props) {
-        super(props);
+const AuthorizeRoute = ({ component, path, ...rest }) => {
+  const [ready, setReady] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
+  const Component = component;
+  const populateAuthenticationState = async () => {
+    const authenticated = await authService.isAuthenticated();
+    setAuthenticated(authenticated);
+    setReady(true);
+  };
+  const authenticationChanged = async () => {
+    setReady(false);
+    setAuthenticated(false);
+    await populateAuthenticationState();
+  };
 
-        this.state = {
-            ready: false,
-            authenticated: false
-        };
-    }
+  useEffect(() => {
+    const subscription = authService.subscribe(() => authenticationChanged());
+    populateAuthenticationState();
+    return () => authService.unsubscribe(subscription);
+  }, []);
 
-    componentDidMount() {
-        this._subscription = authService.subscribe(() => this.authenticationChanged());
-        this.populateAuthenticationState();
-    }
+  var link = document.createElement("a");
+  link.href = path;
+  const returnUrl = `${link.protocol}//${link.host}${link.pathname}${link.search}${link.hash}`;
+  const redirectUrl = `${ApplicationPaths.Login}?${
+    QueryParameterNames.ReturnUrl
+  }=${encodeURIComponent(returnUrl)}`;
+  if (!ready) {
+    return <div></div>;
+  } else {
+    return (
+      <Route
+        {...rest}
+        render={(props) => {
+          if (authenticated) {
+            return <Component {...props} />;
+          } else {
+            return <Redirect to={redirectUrl} />;
+          }
+        }}
+      />
+    );
+  }
+};
 
-    componentWillUnmount() {
-        authService.unsubscribe(this._subscription);
-    }
-
-    render() {
-        const { ready, authenticated } = this.state;
-        var link = document.createElement("a");
-        link.href = this.props.path;
-        const returnUrl = `${link.protocol}//${link.host}${link.pathname}${link.search}${link.hash}`;
-        const redirectUrl = `${ApplicationPaths.Login}?${QueryParameterNames.ReturnUrl}=${encodeURIComponent(returnUrl)}`
-        if (!ready) {
-            return <div></div>;
-        } else {
-            const { component: Component, ...rest } = this.props;
-            return <Route {...rest}
-                render={(props) => {
-                    if (authenticated) {
-                        return <Component {...props} />
-                    } else {
-                        return <Redirect to={redirectUrl} />
-                    }
-                }} />
-        }
-    }
-
-    async populateAuthenticationState() {
-        const authenticated = await authService.isAuthenticated();
-        this.setState({ ready: true, authenticated });
-    }
-
-    async authenticationChanged() {
-        this.setState({ ready: false, authenticated: false });
-        await this.populateAuthenticationState();
-    }
-}
+export default AuthorizeRoute;
