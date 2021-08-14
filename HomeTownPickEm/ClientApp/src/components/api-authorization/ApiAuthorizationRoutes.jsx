8550be70217d413149a8c6c1c7d21cd8b8cd0dde
Drew Killion <adk@killion.me>
2021-08-14 10:44:47 -0400

    making Login a function

--- HomeTownPickEm/ClientApp/src/components/api-authorization/ApiAuthorizationRoutes.jsx
+++ HomeTownPickEm/ClientApp/src/components/api-authorization/ApiAuthorizationRoutes.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { Route } from "react-router";
-import { Login } from "./Login";
+import Login from "./Login";
 import Logout from "./Logout";
 import {
   ApplicationPaths,
