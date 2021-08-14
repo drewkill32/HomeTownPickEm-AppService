825b5efb01526db7e55215dfb218b058c5789434
Drew Killion <adk@killion.me>
2021-08-13 21:10:54 -0400

    updating login

--- HomeTownPickEm/ClientApp/src/components/api-authorization/ApiAuthorizationRoutes.jsx
+++ HomeTownPickEm/ClientApp/src/components/api-authorization/ApiAuthorizationRoutes.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import { Route } from "react-router";
 import { Login } from "./Login";
-import { Logout } from "./Logout";
+import Logout from "./Logout";
 import {
   ApplicationPaths,
   LoginActions,
