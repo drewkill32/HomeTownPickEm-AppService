8c8bb189942d65dbd2280d7b05d215324c4dbbe2
Drew Killion <adk@killion.me>
2021-08-22 08:58:25 -0400

    adding default migrations

--- HomeTownPickEm/Data/DatabaseInit.cs
+++ HomeTownPickEm/Data/DatabaseInit.cs
@@ -1,13 +1,8 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using IdentityServer4.EntityFramework.DbContexts;
-using IdentityServer4.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using IdentityServer4.EntityFramework.Mappers;
-using Client = IdentityServer4.Models.Client;
-using Secret = IdentityServer4.Models.Secret;
 
 namespace HomeTownPickEm.Data
 {
@@ -22,38 +17,20 @@ namespace HomeTownPickEm.Data
 
         public Task Init()
         {
-            return Task.WhenAll(ApplyMigrations<PersistedGrantDbContext>(),
-                ApplyMigrations<ConfigurationDbContext>(),
-                ApplyMigrations<ApplicationDbContext>());
+            return ApplyMigrations<ApplicationDbContext>();
         }
 
-        private async Task ApplyMigrations<TDbContext>() where TDbContext :DbContext
+        public Task Seed()
+        {
+            return Task.CompletedTask;
+        }
+
+        private async Task ApplyMigrations<TDbContext>() where TDbContext : DbContext
         {
             var context = _provider.GetService<TDbContext>();
             if ((await context.Database.GetPendingMigrationsAsync()).Any())
             {
                 await context.Database.MigrateAsync();
-            }
-        }
-
-        public async Task Seed()
-        {
-            var context = _provider.GetService<ConfigurationDbContext>();
-
-            if (!context.Clients.Any())
-            {
-                var client = new Client()
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("SuperSecretPassword".Sha256()) },
-
-                    AllowedScopes = { "weatherapi.read", "weatherapi.write" }
-                };
-                context.Clients.Add(client.ToEntity());
-                await context.SaveChangesAsync();
             }
         }
     }
--- HomeTownPickEm/Program.cs
+++ HomeTownPickEm/Program.cs
@@ -1,32 +1,29 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using HomeTownPickEm.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace HomeTownPickEm
 {
     public class Program
     {
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             using (var scope = host.Services.GetService<IServiceProvider>().CreateScope())
             {
                 var seeder = ActivatorUtilities.CreateInstance<DatabaseInit>(scope.ServiceProvider);
-                //await seeder.Init();
-                //await seeder.Seed();
+                await seeder.Init();
+                await seeder.Seed();
             }
+
             await host.RunAsync();
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
