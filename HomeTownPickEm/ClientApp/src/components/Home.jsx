5aa5d9e119c60a3daeb0cf5ebb0ba3abd4f0a928
Drew Killion <adk@killion.me>
2021-08-22 20:20:27 -0400

    adding weeks

--- HomeTownPickEm/ClientApp/src/components/Home.jsx
+++ HomeTownPickEm/ClientApp/src/components/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import {useAuth} from "../hooks/useAuth";
-import {Redirect} from 'react-router-dom'
+import { useAuth } from "../hooks/useAuth";
+import { Redirect } from "react-router-dom";
 
 const Home = () => {
-  const {user} = useAuth();
+  const { user } = useAuth();
 
-
+  const week = 1;
   if (user) {
-    return (<Redirect to="/picks"/>);
+    return <Redirect to={`/picks/${week}`} />;
   }
-  return (<Redirect to="/login"/>)
+  return <Redirect to="/login" />;
 };
 
 export default Home;
