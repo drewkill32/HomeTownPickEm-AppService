09a83cddad624e8664910abbda83b5d92f2fcf97
Drew Killion <adk@killion.me>
2021-09-02 07:51:46 -0400

    updating cutoff to later in the day

---
+++ HomeTownPickEm/Application/Calendar/Commands/UpdateCalendar.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using HomeTownPickEm.Data;
+using HomeTownPickEm.Extensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeTownPickEm.Application.Calendar.Commands
+{
+    public class UpdateCalendar
+    {
+        public class Command : IRequest<CalendarDto>
+        {
+            public int Week { get; set; }
+            public string Season { get; set; }
+            public string SeasonType { get; set; }
+            public DateTimeOffset? FirstGameStart { get; set; }
+            public DateTimeOffset? LastGameStart { get; set; }
+            public DateTimeOffset? CutoffDate { get; set; }
+        }
+
+        public class CommandHandler : IRequestHandler<Command, CalendarDto>
+        {
+            private readonly ApplicationDbContext _context;
+
+            public CommandHandler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<CalendarDto> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var calendar = (await _context.Calendar.Where(x =>
+                            x.Season == request.Season && x.SeasonType == request.SeasonType
+                                                       && x.Week == request.Week)
+                        .SingleOrDefaultAsync(cancellationToken))
+                    .GuardAgainstNotFound();
+
+                calendar.FirstGameStart = request.FirstGameStart ?? calendar.FirstGameStart;
+                calendar.LastGameStart = request.LastGameStart ?? calendar.LastGameStart;
+                calendar.CutoffDate = request.CutoffDate;
+
+                return calendar.ToCalendarDto();
+            }
+        }
+    }
+}
