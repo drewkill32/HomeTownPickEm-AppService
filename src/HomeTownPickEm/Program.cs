ab3f5e0fda0d9dbacb0a5bc2af2209e5c8f42b20
Drew Killion <dkillion@mmmicrosystems.onmicrosoft.com>
2022-07-17 10:09:41 -0400

    moving startup to extensions methods

--- src/HomeTownPickEm/Program.cs
+++ src/HomeTownPickEm/Program.cs
@@ -1,32 +1,14 @@
-using System.Net.Http.Headers;
-using System.Reflection;
-using System.Text;
-using HomeTownPickEm.Abstract.Interfaces;
-using HomeTownPickEm.Application.Common;
-using HomeTownPickEm.Application.Common.Behaviors;
-using HomeTownPickEm.Config;
 using HomeTownPickEm.Data;
 using HomeTownPickEm.Filters;
-using HomeTownPickEm.Models;
-using HomeTownPickEm.Security;
-using HomeTownPickEm.Services;
-using HomeTownPickEm.Services.Cfbd;
-using HomeTownPickEm.Services.CFBD;
-using HomeTownPickEm.Services.DataSeed;
-using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Builder.Extensions;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-AddDbContext(builder);
+builder.AddDbContext();
 
 
 builder.Services.AddControllers(options =>
@@ -38,14 +20,13 @@ builder.Services.AddControllers(options =>
 
 builder.Services.AddRazorPages();
 
-//TODO: move to extension class
-AddIdentity(builder);
+builder.AddIdentity();
 
-AddJwt(builder);
+builder.AddJwt();
 
-AddSwagger(builder);
+builder.AddSwagger();
 
-AddServices(builder);
+builder.AddServices();
 
 builder.Services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
 
@@ -103,120 +84,6 @@ app.UseSpa(spa =>
 await RunStartup(app);
 
 await app.RunAsync();
-
-void AddDbContext(WebApplicationBuilder webApplicationBuilder)
-{
-    webApplicationBuilder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlite(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection")));
-
-    webApplicationBuilder.Services.AddDatabaseDeveloperPageExceptionFilter();
-}
-
-void AddIdentity(WebApplicationBuilder builder1)
-{
-    var identityBuilder = builder1.Services.AddIdentityCore<ApplicationUser>(opt =>
-    {
-        opt.Password.RequireNonAlphanumeric = false;
-        opt.Password.RequiredLength = 4;
-    });
-    identityBuilder.AddEntityFrameworkStores<ApplicationDbContext>();
-    identityBuilder.AddSignInManager<SignInManager<ApplicationUser>>();
-
-    identityBuilder.AddDefaultTokenProviders();
-}
-
-void AddJwt(WebApplicationBuilder webApplicationBuilder1)
-{
-    webApplicationBuilder1.Services.AddScoped<IJwtGenerator, JwtGenerator>();
-    webApplicationBuilder1.Services.AddScoped<IUserAccessor, UserAccessor>();
-    webApplicationBuilder1.Services.AddHostedService<SeederWorkerService>();
-    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(webApplicationBuilder1.Configuration["TokenKey"]));
-
-    webApplicationBuilder1.Services.AddAuthorization(opt =>
-    {
-        opt.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
-            .RequireAuthenticatedUser()
-            .Build();
-    });
-    webApplicationBuilder1.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
-    {
-        opt.TokenValidationParameters = new ()
-        {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = false,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = key
-        };
-    });
-   
-}
-
-void AddSwagger(WebApplicationBuilder builder2)
-{
-    builder.Services.AddEndpointsApiExplorer();
-    builder2.Services.AddSwaggerGen(opt =>
-    {
-        opt.CustomSchemaIds(t =>
-            t.IsNested ? $"{t.DeclaringType.Name}_{t.Name}" : t.Name);
-        opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-            In = ParameterLocation.Header,
-            Description = "Please enter token",
-            Name = "Authorization",
-            Type = SecuritySchemeType.Http,
-            BearerFormat = "JWT",
-            Scheme = "bearer"
-        });
-
-        opt.AddSecurityRequirement(new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                },
-                new string[] { }
-            }
-        });
-    });
-}
-
-void AddServices(WebApplicationBuilder webApplicationBuilder2)
-{
-    webApplicationBuilder2.Services.AddSeeders();
-    webApplicationBuilder2.Services.AddScoped<ILeagueServiceFactory, LeagueServiceFactory>();
-    webApplicationBuilder2.Services.Configure<SendGridSettings>(
-        webApplicationBuilder2.Configuration.GetSection(SendGridSettings.SettingsKey));
-    webApplicationBuilder2.Services.AddSingleton<IEmailSender, SendGridEmailSender>();
-
-    webApplicationBuilder2.Services.Configure<CfbdSettings>(
-        webApplicationBuilder2.Configuration.GetSection(CfbdSettings.SettingsKey));
-    webApplicationBuilder2.Services.AddHttpClient(CfbdSettings.SettingsKey, (provider, client) =>
-    {
-        var settings = provider.GetRequiredService<IOptions<CfbdSettings>>().Value;
-        client.BaseAddress = new Uri(settings.BaseUrl);
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", settings.Key);
-        client.DefaultRequestHeaders.Add("Accept", "application/json");
-    });
-
-    webApplicationBuilder2.Services.AddHttpClient<ICfbdHttpClient, CfbdHttpClient>((provider, client) =>
-    {
-        var settings = provider.GetRequiredService<IOptions<CfbdSettings>>().Value;
-        client.BaseAddress = new Uri(settings.BaseUrl);
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", settings.Key);
-        client.DefaultRequestHeaders.Add("Accept", "application/json");
-    });
-
-    webApplicationBuilder2.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-    webApplicationBuilder2.Services.AddMediatR(Assembly.GetExecutingAssembly());
-    webApplicationBuilder2.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
-    webApplicationBuilder2.Services.AddScoped<GameTeamRepository>();
-}
 
 async Task RunStartup(WebApplication webApplication)
 {
---
+++ src/HomeTownPickEm/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,148 @@
+﻿using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using HomeTownPickEm.Abstract.Interfaces;
+using HomeTownPickEm.Application.Common;
+using HomeTownPickEm.Application.Common.Behaviors;
+using HomeTownPickEm.Config;
+using HomeTownPickEm.Data;
+using HomeTownPickEm.Models;
+using HomeTownPickEm.Security;
+using HomeTownPickEm.Services;
+using HomeTownPickEm.Services.Cfbd;
+using HomeTownPickEm.Services.CFBD;
+using HomeTownPickEm.Services.DataSeed;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace Microsoft.AspNetCore.Builder.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder AddDbContext(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddIdentity(this WebApplicationBuilder builder)
+    {
+        var identityBuilder = builder.Services.AddIdentityCore<ApplicationUser>(opt =>
+        {
+            opt.Password.RequireNonAlphanumeric = false;
+            opt.Password.RequiredLength = 4;
+        });
+        identityBuilder.AddEntityFrameworkStores<ApplicationDbContext>();
+        identityBuilder.AddSignInManager<SignInManager<ApplicationUser>>();
+
+        identityBuilder.AddDefaultTokenProviders();
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddJwt(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
+        builder.Services.AddScoped<IUserAccessor, UserAccessor>();
+        builder.Services.AddHostedService<SeederWorkerService>();
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"]));
+
+        builder.Services.AddAuthorization(opt =>
+        {
+            opt.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+                .RequireAuthenticatedUser()
+                .Build();
+        });
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+        {
+            opt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = key
+            };
+        });
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddSwagger(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(opt =>
+        {
+            opt.CustomSchemaIds(t =>
+                t.IsNested ? $"{t.DeclaringType.Name}_{t.Name}" : t.Name);
+            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "bearer"
+            });
+
+            opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+        });
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSeeders();
+        builder.Services.AddScoped<ILeagueServiceFactory, LeagueServiceFactory>();
+        builder.Services.Configure<SendGridSettings>(
+            builder.Configuration.GetSection(SendGridSettings.SettingsKey));
+        builder.Services.AddSingleton<IEmailSender, SendGridEmailSender>();
+
+        builder.Services.Configure<CfbdSettings>(
+            builder.Configuration.GetSection(CfbdSettings.SettingsKey));
+        builder.Services.AddHttpClient(CfbdSettings.SettingsKey, (provider, client) =>
+        {
+            var settings = provider.GetRequiredService<IOptions<CfbdSettings>>().Value;
+            client.BaseAddress = new Uri(settings.BaseUrl);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", settings.Key);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+        });
+
+        builder.Services.AddHttpClient<ICfbdHttpClient, CfbdHttpClient>((provider, client) =>
+        {
+            var settings = provider.GetRequiredService<IOptions<CfbdSettings>>().Value;
+            client.BaseAddress = new Uri(settings.BaseUrl);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", settings.Key);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+        });
+
+        builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+        builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+        builder.Services.AddScoped<GameTeamRepository>();
+
+        return builder;
+    }
+}
