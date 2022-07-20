33b93852c800dc68264aca74e44ba439f9967639
Drew Killion <dkillion@mmmicrosystems.onmicrosoft.com>
2022-07-20 19:49:59 -0400

    fixed local storage

--- src/HomeTownPickEm/ClientApp/src/hooks/useLocalStorage.ts
+++ src/HomeTownPickEm/ClientApp/src/hooks/useLocalStorage.ts
@@ -1,13 +1,15 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+
+type SetStateAction<S> = S | ((prevState: S) => S);
 
 export default function useLocalStorage<T>(
   key: string,
   initialValue: T | null
 ): [T, (value: T | null) => void] {
-  const [value, setValue] = useState(() => {
+  const [value, setValue] = useState<T>(() => {
     const jsonValue = localStorage.getItem(key);
     if (jsonValue != null) {
-      return JSON.parse(jsonValue) as T;
+      return JSON.parse(jsonValue);
     }
     return initialValue as T;
   });
