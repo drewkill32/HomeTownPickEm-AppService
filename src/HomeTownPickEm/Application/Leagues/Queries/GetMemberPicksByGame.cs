2445bb9031ce14b19418866ddeb9c6d0b79932d6
Drew Killion <dkillion@mmmicrosystems.onmicrosoft.com>
2022-10-10 08:31:30 -0400

    securing picks to access games before hand

--- src/HomeTownPickEm/Application/Leagues/Queries/GetMemberPicksByGame.cs
+++ src/HomeTownPickEm/Application/Leagues/Queries/GetMemberPicksByGame.cs
@@ -1,3 +1,4 @@
+using HomeTownPickEm.Application.Common;
 using HomeTownPickEm.Data;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -18,14 +19,22 @@ namespace HomeTownPickEm.Application.Leagues.Queries
         public class QueryCommand : IRequestHandler<Query, IEnumerable<UserPickResponse>>
         {
             private readonly ApplicationDbContext _context;
+            private readonly ISystemDate _systemDate;
 
-            public QueryCommand(ApplicationDbContext context)
+            public QueryCommand(ApplicationDbContext context, ISystemDate systemDate)
             {
                 _context = context;
+                _systemDate = systemDate;
             }
 
             public async Task<IEnumerable<UserPickResponse>> Handle(Query request, CancellationToken cancellationToken)
             {
+                var game = await _context.Games.FirstOrDefaultAsync(x => x.Id == request.GameId, cancellationToken);
+
+                if (game.StartDate > _systemDate.UtcNow)
+                {
+                    throw new UnauthorizedAccessException("You can not access the picks before the start of the game");
+                }
                 var users = await (from l in _context.Leaderboard
                         join t in _context.Teams on l.TeamId equals t.Id
                         join u in _context.Users on l.UserId equals u.Id
