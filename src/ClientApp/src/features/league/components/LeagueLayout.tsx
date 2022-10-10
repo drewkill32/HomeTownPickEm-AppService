43c3a90700c6e70d70322438da79040c9577276a
Drew Killion <dkillion@mmmicrosystems.onmicrosoft.com>
2022-10-10 18:20:19 -0400

    adding support to default to current week

--- src/ClientApp/src/features/league/components/LeagueLayout.tsx
+++ src/ClientApp/src/features/league/components/LeagueLayout.tsx
@@ -1,14 +1,26 @@
 import MainLayout from '../../../layout/MainLayout';
 import { useAuth } from '../../authentication';
 import { useLeague } from '../contexts/LeagueProvider';
+import { useCurrentSeason } from '../hooks/useCurrentSeason';
+import { useEffect, useState } from 'react';
 
 const LeagueLayout = ({ children }: { children: JSX.Element }) => {
+  const [week, setWeek] = useState(1);
   const { user } = useAuth();
   const l = useLeague();
+  const query = useCurrentSeason();
+
+  useEffect(() => {
+    setWeek(query.data?.week || 1);
+  }, [query.data]);
+
   const pages = [
     { name: 'Leagues', path: '/leagues' },
     { name: 'Leaderboard', path: `/league/${l.slug}/${l.season}` },
-    { name: 'Picks', path: `/league/${l.slug}/${l.season}/weekly-stats` },
+    {
+      name: 'Picks',
+      path: `/league/${l.slug}/${l.season}/weekly-stats?week=${week}`,
+    },
   ];
   if (user && l && user.claims['commissioner'] === l.id.toString()) {
     pages.push({ name: 'Admin', path: `/league/${l.slug}/${l.season}/admin` });
