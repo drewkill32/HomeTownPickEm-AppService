bf79cb4edb7ddc6b177f51841d12343d920a36e4
Drew Killion <dkillion@mmmicrosystems.onmicrosoft.com>
2022-10-10 18:36:12 -0400

    adding default home screen

--- src/ClientApp/src/features/league/index.ts
+++ src/ClientApp/src/features/league/index.ts
@@ -5,3 +5,6 @@ export { default as Leaderboard } from './pages/Leaderboard';
 export { default as LeagueIndex } from './pages';
 export { LeagueAdmin } from './pages/LeagueAdmin';
 export { WeeklyPicks } from './pages/WeeklyPicks';
+
+export { useGetUserLeagues } from './hooks/useGetUserLeagues';
+export { useCurrentSeason } from './hooks/useCurrentSeason';
--- src/ClientApp/src/pages/HomeSelection.tsx
+++ src/ClientApp/src/pages/HomeSelection.tsx
@@ -1,11 +1,25 @@
 import { useAuth } from '../features/authentication';
 import { LeagueHome } from '../features/league';
+import { useGetUserLeagues, useCurrentSeason } from '../features/league';
+import { Navigate } from 'react-router-dom';
 
 const HomeSelection = () => {
   const { user } = useAuth();
+  const { data: leagues } = useGetUserLeagues();
+  const { data: season } = useCurrentSeason();
 
-  if (!user) {
+  if (!user || !leagues || !season) {
     return null;
+  }
+  console.log({ user, leagues });
+  if (leagues.length === 1) {
+    const l = leagues[0];
+    const year = Math.max(...l.years.map((x) => Number(x)));
+    return (
+      <Navigate
+        to={`/league/${l.slug}/${year}/weekly-stats?week=${season.week}`}
+      />
+    );
   }
   return <LeagueHome />;
 };
