b9949a2207bc80f65455b912af5c43884a07f9f0
Drew Killion <adk@killion.me>
2024-02-13 07:15:24 -0500

    adding shadcn

---
+++ astro/astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+    react(),
+  ],
+});
