14677817f5510e2db4bc0dbf2163d3db23205101
Drew Killion <adk@killion.me>
2024-08-28 18:03:49 -0400

    adding password reset

--- src/HomeTownPickEm/Application/Users/Commands/UnSafelyResetPassword.cs
+++ src/HomeTownPickEm/Application/Users/Commands/UnSafelyResetPassword.cs
@@ -1,58 +1,57 @@
-using System;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using HomeTownPickEm.Extensions;
 using HomeTownPickEm.Models;
+using HomeTownPickEm.Security;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 
-namespace HomeTownPickEm.Application.Users.Commands
+namespace HomeTownPickEm.Application.Users.Commands;
+
+public class UnSafelyResetPassword
 {
-    public class UnSafelyResetPassword
+    public class Command : IRequest
     {
-        public class Command : IRequest
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+
+
+    public class CommandHandler : IRequestHandler<Command>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _config;
+        private readonly IUserAccessor _userAccessor;
+
+
+        public CommandHandler(UserManager<ApplicationUser> userManager, IConfiguration config,
+            IUserAccessor userAccessor)
         {
-            public string Email { get; set; }
-
-            public string Password { get; set; }
-
-            public string Secret { get; set; }
+            _userManager = userManager;
+            _config = config;
+            _userAccessor = userAccessor;
         }
 
-
-        public class CommandHandler : IRequestHandler<Command>
+        public async Task Handle(Command request, CancellationToken cancellationToken)
         {
-            private const string Secret = "This Is My Secret Key. GREG!";
-            private readonly UserManager<ApplicationUser> _userManager;
-
-
-            public CommandHandler(UserManager<ApplicationUser> userManager)
+            var principal = _userAccessor.GetClaimsPrincipal();
+            if (!principal.Claims.Any(c => c is { Type: Claims.Types.Admin, Value: Claims.Values.True }))
             {
-                _userManager = userManager;
+                throw new ForbiddenAccessException("User must be an admin");
             }
 
-            public async Task Handle(Command request, CancellationToken cancellationToken)
+            var user = (await _userManager.FindByEmailAsync(request.Email))
+                .GuardAgainstNotFound("No User Found");
+
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+            var response = await _userManager.ResetPasswordAsync(user, code, request.Password);
+
+            if (response.Succeeded)
             {
-                if (request.Secret != Secret)
-                {
-                    throw new ForbiddenAccessException("Invalid login");
-                }
-
-                var user = (await _userManager.FindByEmailAsync(request.Email))
-                    .GuardAgainstNotFound("No User Found");
-
-                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
-
-                var response = await _userManager.ResetPasswordAsync(user, code, request.Password);
-
-                if (response.Succeeded)
-                {
-                    return;
-                }
-
-                throw new Exception(string.Join(".", response.Errors.Select(x => x.Description)));
+                return;
             }
+
+            throw new(string.Join(".", response.Errors.Select(x => x.Description)));
         }
     }
 }
--- src/HomeTownPickEm/Controllers/UserController.cs
+++ src/HomeTownPickEm/Controllers/UserController.cs
@@ -10,148 +10,160 @@ using Microsoft.AspNetCore.Mvc;
 
 #endregion
 
-namespace HomeTownPickEm.Controllers
+namespace HomeTownPickEm.Controllers;
+
+public class UserController : ApiControllerBase
 {
-    public class UserController : ApiControllerBase
+    [HttpPost]
+    public async Task<ActionResult<UserDto>> AddUser(AddUser.Command command)
     {
-        [HttpPost]
-        public async Task<ActionResult<UserDto>> AddUser(AddUser.Command command)
+        var user = await Mediator.Send(command);
+        return Ok(user);
+    }
+
+    [HttpPost("/api/users")]
+    public async Task<ActionResult<IEnumerable<UserDto>>> AddUsers(AddUsers.Command command)
+    {
+        var users = (await Mediator.Send(command)).ToArray();
+
+        return Ok(users);
+    }
+
+    [HttpGet("{id}", Name = "GetUser")]
+    public async Task<ActionResult<IEnumerable<UserDto>>> Get(string id)
+    {
+        var user = await Mediator.Send(new GetUser.Query
         {
-            var user = await Mediator.Send(command);
-            return Ok(user);
-        }
+            Id = id
+        });
 
-        [HttpPost("/api/users")]
-        public async Task<ActionResult<IEnumerable<UserDto>>> AddUsers(AddUsers.Command command)
+        return Ok(user);
+    }
+
+    [HttpGet("/api/users")]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetAll()
+    {
+        var users = await Mediator.Send(new GetAllUsers.Query());
+
+        return Ok(users);
+    }
+
+    [HttpGet("profile")]
+    public async Task<ActionResult<UserDto>> Profile()
+    {
+        var userId = HttpContext.RequestServices.GetRequiredService<IUserAccessor>().GetCurrentUserId();
+        var user = await Mediator.Send(new GetUser.Query
         {
-            var users = (await Mediator.Send(command)).ToArray();
+            Id = userId
+        });
 
-            return Ok(users);
-        }
+        return Ok(user);
+    }
 
-        [HttpGet("{id}", Name = "GetUser")]
-        public async Task<ActionResult<IEnumerable<UserDto>>> Get(string id)
-        {
-            var user = await Mediator.Send(new GetUser.Query
-            {
-                Id = id
-            });
-
-            return Ok(user);
-        }
-
-        [HttpGet("/api/users")]
-        public async Task<ActionResult<IEnumerable<UserDto>>> GetAll()
-        {
-            var users = await Mediator.Send(new GetAllUsers.Query());
-
-            return Ok(users);
-        }
-
-        [HttpGet("profile")]
-        public async Task<ActionResult<UserDto>> Profile()
-        {
-            var userId = HttpContext.RequestServices.GetRequiredService<IUserAccessor>().GetCurrentUserId();
-            var user = await Mediator.Send(new GetUser.Query
-            {
-                Id = userId
-            });
-
-            return Ok(user);
-        }
-
-        [HttpPost("login")]
-        [AllowAnonymous]
-        public async Task<ActionResult<TokenDto>> Login(Login.Query query)
-        {
-            return await Mediator.Send(query);
-        }
-
-  
-
-        [HttpPost("register")]
-        [AllowAnonymous]
-        public async Task<ActionResult<TokenDto>> Register(Register.Command command)
-        {
-            await Mediator.Send(command);
-            return Ok();
-        }
-
-        [HttpPost("invite")]
-        [AllowAnonymous]
-        public async Task<ActionResult<TokenDto>> Invite(InviteUser.Command command)
-        {
-            await Mediator.Send(command);
-            return new StatusCodeResult(StatusCodes.Status201Created);
-        }
+    [HttpPost("login")]
+    [AllowAnonymous]
+    public async Task<ActionResult<TokenDto>> Login(Login.Query query)
+    {
+        return await Mediator.Send(query);
+    }
 
 
-        [HttpPost("validate")]
-        [AllowAnonymous]
-        public async Task<ActionResult<TokenDto>> Validate(Register.Command command)
-        {
-            await Mediator.Send(command);
-            return Ok();
-        
-        }
+    [HttpPost("register")]
+    [AllowAnonymous]
+    public async Task<ActionResult<TokenDto>> Register(Register.Command command)
+    {
+        await Mediator.Send(command);
+        return Ok();
+    }
 
-        [HttpPost("refresh")]
-        [AllowAnonymous]
-        public async Task<ActionResult<TokenDto>> Refresh(Refresh.Command command)
-        {
-            var user = await Mediator.Send(command);
-            return Ok(user);
-        }
+    [HttpPost("invite")]
+    [AllowAnonymous]
+    public async Task<ActionResult<TokenDto>> Invite(InviteUser.Command command)
+    {
+        await Mediator.Send(command);
+        return new StatusCodeResult(StatusCodes.Status201Created);
+    }
 
-        [HttpPost("logout")]
-        [AllowAnonymous]
-        public async Task<ActionResult> Logout(Logout.Command command)
-        {
-            await Mediator.Send(command);
-            return Accepted();
-        }
+    [HttpPost("resend-invite")]
+    [AllowAnonymous]
+    public async Task<ActionResult<TokenDto>> ResendInvite(ResendInvite.Command command)
+    {
+        await Mediator.Send(command);
+        return new StatusCodeResult(StatusCodes.Status201Created);
+    }
 
-        [HttpPost("resetpassword")]
-        [AllowAnonymous]
-        public async Task<ActionResult> ResetPassword(ResetPassword.Command command)
-        {
-            await Mediator.Send(command);
-            return Ok();
-        }
 
-        [HttpPost("verifyresetpassword")]
-        [AllowAnonymous]
-        public async Task<ActionResult<UserDto>> VerifyResetPassword(VerifyPasswordReset.Command command)
-        {
-            var user = await Mediator.Send(command);
-            return Ok(user);
-        }
+    [HttpPost("validate")]
+    [AllowAnonymous]
+    public async Task<ActionResult<TokenDto>> Validate(Register.Command command)
+    {
+        await Mediator.Send(command);
+        return Ok();
+    }
 
-        [HttpPost("verifyemail")]
-        [AllowAnonymous]
-        public async Task<ActionResult<UserDto>> VerifyEmail(VerifyEmail.Command command)
-        {
-            await Mediator.Send(command);
-            return NoContent();
-        }
-        
+    [HttpPost("refresh")]
+    [AllowAnonymous]
+    public async Task<ActionResult<TokenDto>> Refresh(Refresh.Command command)
+    {
+        var user = await Mediator.Send(command);
+        return Ok(user);
+    }
 
-        [HttpPut("{id}")]
-        public async Task<ActionResult<UserDto>> UpdateUser(string id, UpdateUser.Command command)
-        {
-            command.Id = id;
-            var user = await Mediator.Send(command);
+    [HttpPost("logout")]
+    [AllowAnonymous]
+    public async Task<ActionResult> Logout(Logout.Command command)
+    {
+        await Mediator.Send(command);
+        return Accepted();
+    }
 
-            return Ok(user);
-        }
+    [HttpPost("resetpassword")]
+    [AllowAnonymous]
+    public async Task<ActionResult> ResetPassword(ResetPassword.Command command)
+    {
+        await Mediator.Send(command);
+        return Ok();
+    }
 
-        [HttpDelete("{UserId}")]
-        public async Task<ActionResult<UserDto>> DeleteUser([FromRoute] RemoveUser.Command command)
-        {
-            await Mediator.Send(command);
+    [HttpPost("unsafelyresetpassword")]
+    [AllowAnonymous]
+    public async Task<ActionResult> ResetPassword(UnSafelyResetPassword.Command command)
+    {
+        await Mediator.Send(command);
+        return Ok();
+    }
 
-            return Ok();
-        }
-        
+    [HttpPost("verifyresetpassword")]
+    [AllowAnonymous]
+    public async Task<ActionResult<UserDto>> VerifyResetPassword(VerifyPasswordReset.Command command)
+    {
+        var user = await Mediator.Send(command);
+        return Ok(user);
+    }
+
+    [HttpPost("verifyemail")]
+    [AllowAnonymous]
+    public async Task<ActionResult<UserDto>> VerifyEmail(VerifyEmail.Command command)
+    {
+        await Mediator.Send(command);
+        return NoContent();
+    }
+
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<UserDto>> UpdateUser(string id, UpdateUser.Command command)
+    {
+        command.Id = id;
+        var user = await Mediator.Send(command);
+
+        return Ok(user);
+    }
+
+    [HttpDelete("{UserId}")]
+    public async Task<ActionResult<UserDto>> DeleteUser([FromRoute] RemoveUser.Command command)
+    {
+        await Mediator.Send(command);
+
+        return Ok();
     }
 }
